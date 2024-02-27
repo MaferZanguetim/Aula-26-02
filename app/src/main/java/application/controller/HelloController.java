0d7ded4354237785c1afeb24b274d4d936004772
Mafer Zanguetim <100152834+MaferZanguetim@users.noreply.github.com>
2024-02-27 17:56:41 +0000

    Primeiro Controller

--- app/src/main/java/application/controller/HelloController.java
+++ app/src/main/java/application/controller/HelloController.java
@@ -2,10 +2,12 @@ package application.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class HelloController{
     @GetMapping("/teste")
+    @ResponseBody
     public String teste(){
         return "Olá Mundo";
     }
